510140f7e5a1e8629e6c016d94165626a5f56b18
AntonStoeckl <anton@stoeckl.de>
2016-05-15 23:04:50 +0200

    fix namespace

--- src/fiunchinho/Silex/Command/Consumer.php
+++ src/fiunchinho/Silex/Command/Consumer.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Knp\Command\Command as BaseCommand;
+use Ivoba\Silex\Command\Command as BaseCommand;
 
 class Consumer extends BaseCommand
 {
